d672d270a0fca686b117e273e6a23c03083d182f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-20 20:56:23 +0530

    useing useEffect in api for mounting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { getPosts, getRandomUser } from './Api';
 import './App.css';
+import { PostCard } from './components/PostCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data,setData] = useState(null)
+  useEffect(()=>{
+    getPosts().then((posts) => setData(posts));
+  },[]);
+
+  useEffect(()=>{
+    getRandomUser().then((user) => console.log(user))
+  },[])
+
+  return <div className='App'>
+  {
+   data 
+     ? data.map((e) => <PostCard  title={e.title} body = {e.body}/>) : <p> NO DATA </p>
+  }
+  </div>
 }
 
 export default App;
