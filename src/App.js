ac47bb93ab2b78fb1613c41c5402138147cc9f5c
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-20 21:38:08 +0530

    updating data use of button

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import { useEffect, useState } from 'react';
 import { getPosts, getRandomUser } from './Api';
 import './App.css';
 import { PostCard } from './components/PostCard';
+import { UserCard } from './components/UserCard';
 
 function App() {
     const [data,setData] = useState(null)
+    const [userdata, setUserdata] = useState(null)
   useEffect(()=>{
     getPosts().then((posts) => setData(posts));
   },[]);
 
   useEffect(()=>{
-    getRandomUser().then((user) => console.log(user))
+    getRandomUser().then((user) => setUserdata(user.results[0]))
   },[])
+  const refresh = ()=>{
+    getRandomUser().then((user) => setUserdata(user.results[0]))
+  }
 
   return <div className='App'>
+   {userdata && <UserCard  data = {userdata}/>}
+   <button onClick={refresh}>Refresh me</button>
   {
    data 
      ? data.map((e) => <PostCard  title={e.title} body = {e.body}/>) : <p> NO DATA </p>
