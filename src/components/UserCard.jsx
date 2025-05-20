c95488a1f866e00013e1a0a0a4c2ecee49051a8a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-20 23:12:08 +0530

    add

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,7 +4,11 @@ import React from "react"
   console.log(props.data)
   return (
   <div className="user-card">
-      <img  src="" alt="" />
+       <img
+        src={props.data.picture?.large}
+        alt={`${props.data.name?.first} ${props.data.name?.last}`}
+        className="user-avatar"
+      />
       <h3>{props.data.name.first}</h3>
       <p>{props.data.phone}</p>
       <p>{props.data.location.city},{props.data.location.state}</p>
